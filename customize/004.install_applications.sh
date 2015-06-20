28527bc58ac67c590ce4da799d61b6190e3698ef
Zeal <xujinzheng@gmail.com>
2015-06-20 22:32:32 +0800

    add sublimetext 3 support

--- customize/004.install_applications.sh
+++ customize/004.install_applications.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-brew cask install 'charles' 'macdown' 'goagentx' 'evernote' 'filezilla' 'google-chrome' 'mysqlworkbench' 'scroll-reverser' 'evernote' 'iterm2'
+brew tap caskroom/versions
+brew cask install 'charles' 'macdown' 'goagentx' 'evernote' 'filezilla' 'google-chrome' 'mysqlworkbench' 'scroll-reverser' 'evernote' 'iterm2' 'sublime-text3'
